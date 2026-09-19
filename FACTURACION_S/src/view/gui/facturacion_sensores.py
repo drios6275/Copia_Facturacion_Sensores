df07c7930767b043630166dfb3fd1de02707b2e7
David Rios <yosoydrios@gmail.com>
2026-09-19 14:06:54 -0500

    agregue el manejo de errores basicos sin popau

--- FACTURACION_S/src/view/gui/facturacion_sensores.py
+++ FACTURACION_S/src/view/gui/facturacion_sensores.py
@@ -13,13 +13,15 @@ from model.logica_sensores import calcular_valor_factura
 
 class FacturacionSensoresApp(App):
     def build(self):
-        contenedor = GridLayout(rows = 3,cols = 2)
+        contenedor = GridLayout(rows = 4,cols = 2)
         etiqueta1 = Label(text = "Numero servicios")
         etiqueta2 = Label(text = "Precio Unitario")
         self.numero_servicio = TextInput()
         self.precio_unitario = TextInput()
         boton_calcular = Button(text = "Calcular factura")
         self.resultado = Label(text = "Aqui va el resultado")
+        contenedor.add_widget(Label(text="Bienvenido a la Calculador de Sensores" , font_size = 20))
+        contenedor.add_widget(Label(text = "Ingrese el Numero de servicio y \n el Precio unitario de la factura"))
 
         contenedor.add_widget(etiqueta1)
         contenedor.add_widget(self.numero_servicio)
@@ -34,14 +36,23 @@ class FacturacionSensoresApp(App):
 
         return contenedor
 
+    def validar(self):
+        if not self.numero_servicio .text.isnumeric():
+            raise Exception (" Debe ingresar un numero valido en el numero de servicios")
+        if not self.precio_unitario.text.isnumeric():
+            raise Exception("Debe de ingresar un numero valido en el precio unitario")
+
 
     def calcular_factura(self,sender):
-
-        numero_servicio: int = int(self.numero_servicio.text)
-        precio_unitario: float = float(self.precio_unitario.text)
-        resultado = calcular_valor_factura(numero_servicio,precio_unitario)
-        self.resultado.text = str(resultado)
-
+        try:
+            self.validar()
+            numero_servicio: int = int(self.numero_servicio.text)
+            precio_unitario: float = float(self.precio_unitario.text)
+        
+            resultado = calcular_valor_factura(numero_servicio,precio_unitario)
+            self.resultado.text = str(resultado)
+        except Exception as error:
+            self.resultado.text = "No se puede calcular \n"  + str (error)
 
 
 
